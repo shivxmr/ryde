cf802e60a46a96f1fc5568ea8a4b7d401e4d112c
Shivam Raj <shivamraj.me21@rvce.edu.in>
2024-08-19 01:20:54 +0530

    neon db & api

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,10 +1,44 @@
-import { Text, View } from "react-native";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { useAuth } from "@clerk/clerk-expo";
+import * as Location from "expo-location";
+import { Link, router } from "expo-router";
+import { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
+  const { user } = useUser();
+  const { signOut } = useAuth();
+
+  const handleSignOut = () => {
+    signOut();
+    router.replace("/(auth)/sign-in");
+  };
+
+  const [hasPermission, setHasPermission] = useState<boolean>(false);
+
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView className="bg-general-500">
+      <View>
+        <SignedIn>
+          <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        </SignedIn>
+        <SignedOut>
+          <Link href="/sign-in">
+            <Text>Sign In</Text>
+          </Link>
+          <Link href="/sign-up">
+            <Text>Sign Up</Text>
+          </Link>
+        </SignedOut>
+      </View>
     </SafeAreaView>
   );
 };
